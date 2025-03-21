09db5ad960406ae75a22134c3050fa24625903f4
Aapka Naam <aapkaemail@example.com>
2025-03-21 08:19:02 +0530

    [Sweta] Add: Added code to the branch

---
+++ AddressBook.js
@@ -0,0 +1,13 @@
+// UC1: Ability to create an Address Book Contact with first and last names, address, city, state, zip, phone number and email
+class AddressBookContact {
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
